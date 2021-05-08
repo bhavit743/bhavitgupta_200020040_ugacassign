b377af29e41cda1af8dc9f736546c8da21c865c1
Bhavit Gupta <bhavitgupta743@gmail.com>
2021-05-08 17:31:04 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,18 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {rollnumber: 200010001, name: 'Bhavit Gupta', branch: 'Civil Engineering'},
-  {rollnumber: 200020002, name: 'Bhavit Gupta', branch: 'Chemical Engineering'},
-  {rollnumber: 200030003, name: 'Bhavit Gupta', branch: 'CSE'},
-  {rollnumber: 200040004, name: 'Bhavit Gupta', branch: 'Electrical Engineering'},
-  {rollnumber: 200050005, name: 'Bhavit Gupta', branch: 'Mechanical Engineering'},
-  {rollnumber: 200060006, name: 'Bhavit Gupta', branch: 'MEMS'},
-  {rollnumber: 200070007, name: 'Bhavit Gupta', branch: 'Aerospace Engineering'},
-  {rollnumber: 200080008, name: 'Bhavit Gupta', branch: 'Economics'},
-  {rollnumber: 200090009, name: 'Bhavit Gupta', branch: 'Maths Engineering'},
-  {rollnumber: 200010010, name: 'Bhavit Gupta', branch: 'Civil Engineering'},
-  {rollnumber: 200020011, name: 'Bhavit Gupta', branch: 'Chemical Engineering'},
-  {rollnumber: 200030012, name: 'Bhavit Gupta', branch: 'CSE'}
+  {rollnumber: 200010001, name: 'Student-1', branch: 'Civil Engineering'},
+  {rollnumber: 200020002, name: 'Student-2', branch: 'Chemical Engineering'},
+  {rollnumber: 200030003, name: 'Student-3', branch: 'CSE'},
+  {rollnumber: 200040004, name: 'Student-4', branch: 'Electrical Engineering'},
+  {rollnumber: 200050005, name: 'Student-5', branch: 'Mechanical Engineering'},
+  {rollnumber: 200060006, name: 'Student-6', branch: 'MEMS'},
+  {rollnumber: 200070007, name: 'Student-7', branch: 'Aerospace Engineering'},
+  {rollnumber: 200080008, name: 'Student-8', branch: 'Economics'},
+  {rollnumber: 200090009, name: 'Student-9', branch: 'Maths Engineering'},
+  {rollnumber: 200010010, name: 'Student-10', branch: 'Civil Engineering'},
+  {rollnumber: 200020011, name: 'Student-11', branch: 'Chemical Engineering'},
+  {rollnumber: 200030012, name: 'Student-12', branch: 'CSE'}
 ];
 
 @Component({
